2ac219b40cfc378b30a7c4ae814288c42c25b668
jzy01309237 <jzy01309237@alibaba-inc.com>
2022-05-24 10:49:54 +0800

    feat: 添加路由

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
 
 Vue.config.productionTip = false
 
+if(window.dd) {
+  window.dd.enableUrlChangeDetect = true
+}
+
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
